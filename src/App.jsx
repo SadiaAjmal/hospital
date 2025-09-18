45789af0c19843c1efc5d71d72d64d04d860d686
ZAIN Ul ABAIDEEN <119429395+zainulabaideen@users.noreply.github.com>
2025-09-19 00:39:47 +0500

    mui is also added

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import About from "./Pages/About/About"
 import Contact from "./Pages/Contact/Contact"
 import Home from "./Pages/Home/Home"
+import Navbar from "./Components/Navbar/Navbar"
+import Footer from "./Components/Footer/Footer"
 
 function App() {
 
   return (
     <>
 
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
